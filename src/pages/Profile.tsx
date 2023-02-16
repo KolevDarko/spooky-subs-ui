33f5b35367fffee4aae14711d5c292a7b394325b
Zoran Stoilov <zoranstoilov@yahoo.com>
2023-02-16 09:10:48 +0100

    Added wagmi and integrated it in profile.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,7 +3,8 @@ import {
   CheckCircleIcon, 
   EmailIcon, 
   StarIcon, 
-  WarningIcon 
+  WarningIcon,
+  LockIcon,
 } from '@chakra-ui/icons'
 import { 
   Tabs, 
@@ -15,15 +16,31 @@ import {
   ListItem, 
   ListIcon 
 } from '@chakra-ui/react'
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+} from 'wagmi'
 
 export default function Profile() {
+    const { address, connector, isConnected } = useAccount()
+    const { data: ensAvatar } = useEnsAvatar({ address })
+    const { data: ensName } = useEnsName({ address })
+    const { connect, connectors, error, isLoading, pendingConnector } =
+      useConnect()
+    const { disconnect } = useDisconnect()
+
+
+
   return (
     <Tabs mt="40px" p="20px" variant="enclosed" colorScheme="purple">
       <TabList>
         <Tab _selected={{ color: 'white', bg: 'purple.400' }}>Account Info</Tab>
         <Tab _selected={{ color: 'white', bg: 'purple.400' }}>Task History</Tab>
+        <Tab _selected={{ color: 'white', bg: 'purple.400' }}>Wallet</Tab>
       </TabList>
-
       <TabPanels py="10px">
         <TabPanel>
           <List spacing={4}>
@@ -67,6 +84,35 @@ export default function Profile() {
               <ListIcon as={CheckCircleIcon} color="teal.400" />
               Lorem ipsum dolor sit amet consectetur.
             </ListItem>
+          </List>
+        </TabPanel>
+        <TabPanel>
+        <List spacing={4}>
+          {
+          isConnected ?         
+          <ListItem>
+            <img src={ensAvatar!} alt="ENS Avatar" />
+            <div>{ensName ? `${ensName} (${address})` : address}</div>
+            <div>Connected to {connector?.name}</div>
+            <button onClick={() => disconnect()}>Disconnect</button>
+          </ListItem>
+        :
+            connectors.map((connector) => (
+              <ListItem>
+              <button
+                disabled={!connector.ready}
+                key={connector.id}
+                onClick={() => connect({ connector })}
+              >
+                {connector.name}
+                {!connector.ready && ' (unsupported)'}
+                {isLoading &&
+                  connector.id === pendingConnector?.id &&
+                  ' (connecting)'}
+              </button>
+              </ListItem>
+            ))}
+            {error && <div>{error.message}</div>}
           </List>
         </TabPanel>
       </TabPanels>
