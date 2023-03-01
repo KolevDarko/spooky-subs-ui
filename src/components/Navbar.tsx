c0b30ebc15167de826d60481a443e9609a5e763e
Darko Kolev <kolevdarko@gmail.com>
2023-03-01 16:41:32 +0100

    axios

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,15 @@
-import { UnlockIcon } from "@chakra-ui/icons"
 import { 
   Flex, 
   Heading, 
   Text, 
-  Button, 
   Spacer, 
   HStack, 
-  useToast, 
   AvatarBadge,
   Avatar
 } from "@chakra-ui/react"
 import { ConnectKitButton } from "connectkit"
 
 export default function Navbar() {
-  const toast = useToast()
-
   return (
     <Flex as="nav" p="10px" mb="60px" alignItems="center">
       <Heading as="h1" fontSize="1.5em">Dojo Tasks</Heading>
@@ -26,20 +21,7 @@ export default function Navbar() {
             <Text fontSize="xs" color="white">3</Text>
           </AvatarBadge>
         </Avatar>
-        <Text>mario@netninja.dev</Text>
         <ConnectKitButton label="Connect Wallet" />
-        <Button 
-          colorScheme="purple"
-          onClick={() => toast({
-            title: 'Logged out.',
-            description: "Successfully logged out",
-            duration: 10000,
-            isClosable: true,
-            position: 'top',
-            status: 'success',
-            icon: <UnlockIcon />,
-          })}
-        >Logout</Button>
       </HStack>
     </Flex>
   )
